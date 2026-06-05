f18a86ea03e543f8a3fc1ea8bdf0bd469b0a1adc
MarcosTavar3s <maurelio1411@gmail.com>
2026-06-05 20:39:03 -0300

    insertion sort

---
+++ sorting_algorithms/insertion_sort/insertion_sort.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func insertionSort(v []int) {
+
+	for i := 0; i < len(v); i++ {
+		for j := i; j > 0 && v[j-1] > v[j]; j-- {
+			v[j], v[j-1] = v[j-1], v[j]
+		}
+
+	}
+}
+
+func generateVector(size int) []int {
+	v := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		v[i] = rand.IntN(100)
+	}
+
+	return v
+}
+
+func printVector(v []int) {
+	for i := 0; i < len(v); i++ {
+		if i == 0 {
+			fmt.Printf("[ %v", v[i])
+		} else if i == len(v)-1 {
+			fmt.Printf("%v ]\n", v[i])
+		} else {
+			fmt.Printf(" %v ", v[i])
+		}
+
+	}
+}
+
+func main() {
+	fmt.Println("Selection sort algorithm")
+
+	size := 50
+	v := generateVector(size)
+	printVector(v)
+	insertionSort(v)
+	printVector(v)
+}
