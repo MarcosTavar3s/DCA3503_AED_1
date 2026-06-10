3ddef48a37e65c1e70f35717b39e6857f8bdc896
MarcosTavar3s <maurelio1411@gmail.com>
2026-06-10 20:01:33 -0300

    remove, height

--- bst/bst.go
+++ bst/bst.go
@@ -42,17 +42,210 @@ func search(bst *Bst, val int) bool {
 
 }
 
-// func (bst *Bst) PreOrder(val int) {}
+func (bst *Bst) PreOrder() {
+
+	if bst == nil {
+		return
+	}
+
+	println(bst.val)
+	bst.esquerda.PreOrder()
+	bst.direita.PreOrder()
+
+}
+
+func (bst *Bst) InOrder() {
+
+	if bst == nil {
+		return
+	}
+	bst.esquerda.InOrder()
+	println(bst.val)
+	bst.direita.InOrder()
+
+}
+
+func (bst *Bst) PostOrder() {
+	if bst == nil {
+		return
+	}
+
+	bst.esquerda.PostOrder()
+	bst.direita.PostOrder()
+	println(bst.val)
+}
+
+func (bst *Bst) LevelOrder() {
+	queue := make([]*Bst, 0)
+	queue = append(queue, bst)
+
+	for len(queue) != 0 {
+		curNode := queue[0]
+
+		println(curNode.val)
+
+		if curNode.esquerda != nil {
+			queue = append(queue, curNode.esquerda)
+		}
+		if curNode.direita != nil {
+			queue = append(queue, curNode.direita)
+		}
+
+		queue = queue[1:]
+	}
+}
+
+func (bst *Bst) Min() int {
+	if bst.esquerda == nil {
+		return bst.val
+	}
+
+	return bst.esquerda.Min()
+}
+
+func (bst *Bst) Max() int {
+	if bst.direita == nil {
+		return bst.val
+	}
+
+	return bst.direita.Max()
+}
+
+func (bst *Bst) Size() int {
+	if bst == nil {
+		return 0
+	} else {
+		return 1 + bst.direita.Size() + bst.esquerda.Size()
+	}
+
+}
+
+func (bst *Bst) Par() int {
+	if bst == nil {
+		return 0
+	} else {
+		if bst.val%2 == 0 {
+			return 1 + bst.direita.Par() + bst.esquerda.Par()
+		}
+		return bst.direita.Par() + bst.esquerda.Par()
+	}
+}
+
+func (bst *Bst) Impar() int {
+	if bst == nil {
+		return 0
+	} else {
+		if bst.val%2 == 1 {
+			return 1 + bst.direita.Impar() + bst.esquerda.Impar()
+		}
+		return bst.direita.Impar() + bst.esquerda.Impar()
+	}
+}
+
+func convertToBalancedBst(v []int, ini int, fim int) *Bst {
+	if ini > fim {
+		return nil
+	}
+
+	meio := (ini + fim) / 2
+
+	novoBst := Bst{val: v[meio]}
+
+	novoBst.direita = convertToBalancedBst(v, meio+1, fim)
+	novoBst.esquerda = convertToBalancedBst(v, ini, meio-1)
+
+	return &novoBst
+}
+
+func (bst *Bst) Height() int {
+	heightLeft := 0
+	heightRight := 0
+
+	if bst == nil {
+		return 0
+	}
+
+	if bst.direita != nil {
+		heightRight = 1 + bst.direita.Height()
+	}
+
+	if bst.esquerda != nil {
+		heightLeft = 1 + bst.esquerda.Height()
+	}
+
+	if heightLeft > heightRight {
+		return heightLeft
+	}
+	return heightRight
+}
+
+func (bst *Bst) Remove(val int) *Bst {
+
+	// achou o valor
+	if bst.val == val {
+		if bst.direita == nil && bst.esquerda == nil { // caso1: folha
+			return nil
+		}
+		if bst.direita == nil && bst.esquerda != nil { // caso2: tem 1 filho à esquerda
+			return bst.esquerda
+		}
+		if bst.direita != nil && bst.esquerda == nil { // caso2: tem 1 filho à direita
+			return bst.direita
+		}
+		if bst.direita != nil && bst.esquerda != nil { // caso3: tem 2 filhos
+			min := bst.direita.Min()
+			bst.val = min
+			bst.direita = bst.direita.Remove(min)
+			return bst
+		}
+	} else {
+		if bst.val > val {
+			bst.esquerda = bst.esquerda.Remove(val)
+		} else {
+			bst.direita = bst.direita.Remove(val)
+		}
+	}
+	return bst
+
+}
+
+// func (bst *Bst) isBst() bool {
+// usar percurso em ordem -> mais complexo mas resolve
+// }
 
 func main() {
-	var bst Bst
+	// cria a primeira raiz
+	bst := &Bst{val: 13}
 
-	Add(&bst, 13)
-	Add(&bst, 15)
-	Add(&bst, 7)
-	Add(&bst, 6)
-	Add(&bst, 8)
-	Add(&bst, 20)
-	Add(&bst, 14)
-	println(search(&bst, 13))
+	// Add(&bst, 13)
+	Add(bst, 15)
+	Add(bst, 7)
+	Add(bst, 6)
+	Add(bst, 8)
+	Add(bst, 20)
+	Add(bst, 14)
+	Add(bst, 9)
+	Add(bst, 10)
+
+	// bst.InOrder()
+	// bst.PreOrder()
+	// bst.PostOrder()
+	// bst.LevelOrder()
+
+	println("Minimo:", bst.Min())
+	println("Maximo:", bst.Max())
+	println("Tamanho:", bst.Size())
+	println("Pares:", bst.Par())
+	println("Impares:", bst.Impar())
+	bst = bst.Remove(13)
+	println("Altura:", bst.Height())
+	bst.LevelOrder()
+
+	// a := make([]int, 10)
+	// for i := 0; i < 10; i++ {
+	// 	a[i] = i * 10
+	// }
+
+	// novoBst := convertToBalancedBst(a, 0, len(a)-1)
+	// novoBst.PreOrder()
 }
